345706147a21fade9ad0ed4ab4b09b108d39269f
Hossen <mdmitho199911@gmail.com>
2022-07-28 23:32:41 +0600

    display data name etc

--- src/Components/DispalyInfo/DispalyInfo.js
+++ src/Components/DispalyInfo/DispalyInfo.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 const DispalyInfo = (props) => {
 
-    console.log(props.info.show.id);
+    const img = props.info.show.image.medium;
+    const name = props.info.show.name;
+    const language = props.info.show.language;
+
+    console.log(props.info.show.language);
 
     return (
-        <div>
-            
+      <div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={img} alt="Shoes" />
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">Name : {name}</h2>
+            <p>Language : {language}</p>
+            <div class="card-actions">
+              <button class="btn btn-primary text-white">Show More Details</button>
+            </div>
+          </div>
         </div>
+
+        </div>
+
+       
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,24 +6,13 @@ const Home = () => {
     const [infos]=FatchData()
     
     return (
-        <div>
-
-
-
-            
-       {
-        infos.map((info)=>(
-            <DispalyInfo info={info}
-            key={info.show.id}
-            >
-
-            </DispalyInfo>
-        )
-
-        )
-       }
-            
+      <div>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 m-5 justify-center">
+          {infos.map((info) => (
+            <DispalyInfo info={info} key={info.show.id}></DispalyInfo>
+          ))}
         </div>
+      </div>
     );
 };
 
